898cf2f5a66f7f6e5c3a848478ec12f00f13057b
i6nake9 <fleshartemka@gmail.com>
2023-01-21 13:57:29 +0200

    add mongo config & auth model

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TopPageController } from './top-page.controller';
 
 @Module({
   controllers: [TopPageController],
 })
 export class TopPageModule {
-  constructor(private readonly config: ConfigService) {
-    this.config.get('Test');
-  }
+  constructor() {}
 }
